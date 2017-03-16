0e87a9794c962da54f9834f057f72f52d8152661
Jeff Kelley <SlaunchaMan@gmail.com>
2017-03-16 18:11:20 -0400

    Add audio unit

---
+++ MyAudioUnit/MyAudioUnit-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  MyAudioUnit-Bridging-Header.h
+//  MyAudioUnit
+//
+//  Created by Jeff Kelley on 3/16/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+#import "MyAudioUnitAudioUnit.h"
---
+++ MyAudioUnit/MyAudioUnitAudioUnit.h
@@ -0,0 +1,16 @@
+//
+//  MyAudioUnitAudioUnit.h
+//  MyAudioUnit
+//
+//  Created by Jeff Kelley on 3/16/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+#import <AudioToolbox/AudioToolbox.h>
+
+// Define parameter addresses.
+extern const AudioUnitParameterID myParam1;
+
+@interface MyAudioUnitAudioUnit : AUAudioUnit
+
+@end
