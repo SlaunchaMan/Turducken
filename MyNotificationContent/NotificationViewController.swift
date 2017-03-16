7bc7b30e9b5b0254bc1dfc3dd06a053985c34af3
Jeff Kelley <SlaunchaMan@gmail.com>
2017-03-16 18:47:06 -0400

    Add Notification Content

---
+++ MyNotificationContent/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  MyNotificationContent
+//
+//  Created by Jeff Kelley on 3/16/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
