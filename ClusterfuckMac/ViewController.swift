779914d1e6780a65adcc4cf4757582aae7ea62df
Jeff Kelley <SlaunchaMan@gmail.com>
2017-03-16 19:15:50 -0400

    Add macOS project.

---
+++ ClusterfuckMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClusterfuckMac
+//
+//  Created by Jeff Kelley on 3/16/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
