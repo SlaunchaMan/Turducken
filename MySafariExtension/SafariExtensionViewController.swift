c59ad2a08b3f9124acaea916a8942968d0876f59
Jeff Kelley <SlaunchaMan@gmail.com>
2017-03-16 20:36:19 -0400

    Add Safari extension

---
+++ MySafariExtension/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  MySafariExtension
+//
+//  Created by Jeff Kelley on 3/16/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
