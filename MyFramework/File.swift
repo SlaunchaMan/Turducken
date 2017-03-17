90fc65a77eab5e0d2654e99ee9c05aa85f836d41
Jeff Kelley <SlaunchaMan@gmail.com>
2017-03-16 20:57:24 -0400

    Add framework targets.

---
+++ MyFramework/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Clusterfuck
+//
+//  Created by Jeff Kelley on 3/16/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+import Foundation
---
+++ MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Jeff Kelley on 3/16/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
