227ed9e7af689ae7f9169569b56b6c54a8d22082
Jeff Kelley <SlaunchaMan@gmail.com>
2017-06-17 07:57:20 -0400

    Add File Provider extension

---
+++ MyFileProvider/FileProviderItem.swift
@@ -0,0 +1,36 @@
+//
+//  FileProviderItem.swift
+//  MyFileProvider
+//
+//  Created by Jeff Kelley on 6/17/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+import FileProvider
+
+class FileProviderItem: NSObject, NSFileProviderItem {
+
+    // TODO: implement an initializer to create an item from your extension's backing model
+    // TODO: implement the accessors to return the values from your extension's backing model
+    
+    var itemIdentifier: NSFileProviderItemIdentifier {
+        return NSFileProviderItemIdentifier("")
+    }
+    
+    var parentItemIdentifier: NSFileProviderItemIdentifier {
+        return NSFileProviderItemIdentifier("")
+    }
+    
+    var capabilities: NSFileProviderItemCapabilities {
+        return .allowsAll
+    }
+    
+    var filename: String {
+        return ""
+    }
+    
+    var typeIdentifier: String {
+        return ""
+    }
+    
+}
