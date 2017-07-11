d2d2e2d00db3ba5941dd037945051064c5b94b4d
Jeff Kelley <SlaunchaMan@gmail.com>
2017-07-11 01:35:37 -0400

    Add static library.

---
+++ MyStaticLibrary/MyStaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  MyStaticLibrary.h
+//  MyStaticLibrary
+//
+//  Created by Jeff Kelley on 7/11/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyStaticLibrary : NSObject
+
+@end
---
+++ MyStaticLibrary/MyStaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  MyStaticLibrary.m
+//  MyStaticLibrary
+//
+//  Created by Jeff Kelley on 7/11/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+#import "MyStaticLibrary.h"
+
+@implementation MyStaticLibrary
+
+@end
